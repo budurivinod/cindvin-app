a9d3cea7374df226089c7513c3c83ea404dac25f
budurivinod <budurivinod@gmail.com>
2026-02-18 23:05:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,13 @@
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            if (windowClients.length > 0) {
-                return windowClients[0].focus();
-            }
-            return clients.openWindow('/');
-        })
-    );
-});
-
-// Listener for background pushes
 self.addEventListener('push', function(event) {
     const options = {
-        body: 'Your partner updated something in HealthSync!',
+        body: 'New update from your partner! ❤️',
         icon: 'https://cdn-icons-png.flaticon.com/512/3105/3105807.png',
         vibrate: [200, 100, 200]
     };
     event.waitUntil(self.registration.showNotification('HealthSync Pro', options));
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/'));
 });
