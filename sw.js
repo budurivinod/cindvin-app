9612e12d39bdd86d4993751f7242064e49c8ade1
budurivinod <budurivinod@gmail.com>
2026-02-18 22:58:51 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,26 +2,20 @@ self.addEventListener('notificationclick', function(event) {
     event.notification.close();
     event.waitUntil(
         clients.matchAll({ type: 'window' }).then(windowClients => {
-            for (var i = 0; i < windowClients.length; i++) {
-                var client = windowClients[i];
-                if (client.url === '/' && 'focus' in client) {
-                    return client.focus();
-                }
+            if (windowClients.length > 0) {
+                return windowClients[0].focus();
             }
-            if (clients.openWindow) {
-                return clients.openWindow('/');
-            }
+            return clients.openWindow('/');
         })
     );
 });
 
-// Listener for pushes (if you integrate Firebase Cloud Messaging later)
+// Listener for background pushes
 self.addEventListener('push', function(event) {
-    const data = event.data.json();
     const options = {
-        body: data.body,
+        body: 'Your partner updated something in HealthSync!',
         icon: 'https://cdn-icons-png.flaticon.com/512/3105/3105807.png',
-        badge: 'https://cdn-icons-png.flaticon.com/512/3105/3105807.png'
+        vibrate: [200, 100, 200]
     };
-    event.waitUntil(self.registration.showNotification(data.title, options));
+    event.waitUntil(self.registration.showNotification('HealthSync Pro', options));
 });
