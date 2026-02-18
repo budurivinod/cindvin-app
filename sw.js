aa92bc2911639265f93ba136ca43cb3c5cac65df
budurivinod <budurivinod@gmail.com>
2026-02-18 22:52:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
-    event.waitUntil(clients.openWindow(event.notification.data.url));
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(windowClients => {
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                if (client.url === '/' && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
 });
 
+// Listener for pushes (if you integrate Firebase Cloud Messaging later)
 self.addEventListener('push', function(event) {
     const data = event.data.json();
     const options = {
         body: data.body,
         icon: 'https://cdn-icons-png.flaticon.com/512/3105/3105807.png',
-        data: { url: self.registration.scope }
+        badge: 'https://cdn-icons-png.flaticon.com/512/3105/3105807.png'
     };
     event.waitUntil(self.registration.showNotification(data.title, options));
 });
